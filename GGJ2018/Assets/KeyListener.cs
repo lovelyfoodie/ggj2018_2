4bfe37e1b78ee5e83d05bdbdfbcd5142a45cfc92
EnkiduRed <rbrolley@alchemedium.com>
2018-01-28 13:00:06 -0500

    start game

---
+++ GGJ2018/Assets/KeyListener.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class KeyListener : MonoBehaviour
+{
+    public List<string> keys;
+    public UnityEvent OnPress;
+
+    void Update()
+    {
+        foreach (var item in keys)
+        {
+            if (Input.GetButton(item))
+            {
+                OnPress.Invoke();
+            }
+        }
+    }
+}
