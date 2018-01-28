ecd345f562d89a679d56e350f57e9edc71f3708a
EnkiduRed <rbrolley@alchemedium.com>
2018-01-28 10:39:08 -0500

    fixed bucket issue, added additional gates

--- GGJ2018/Assets/Gates.cs
+++ GGJ2018/Assets/Gates.cs
@@ -6,35 +6,54 @@ using System;
 
 public class Gates : MonoBehaviour
 {
-    
+    public bool flipControls = false;
+
+    [Header("Gate A")]
     [SerializeField]
     private string ActivateGateA = "Fire1";
+    public Color ColorA = Color.blue;
+    public int passLayerA = 8;
+    public int blockLayerA = 12;
+
+    [Header("Gate B")]
     [SerializeField]
     private string ActivateGateB = "Fire2";
-
-    public Color ColorA = Color.blue;
     public Color ColorB = Color.red;
-    public Color ColorDefault = Color.white;
+    public int passLayerB = 9;
+    public int blockLayerB = 13;
 
+    [Header("Default State")]
+    public Color ColorDefault = Color.white;
+    public int passLayerDefault = 0;
+    public int blockLayerDefault = 14;
+
+    [Header("GameObject Refs")]
     public Collider2D gate;
+    public Collider2D blocker;
     public SpriteRenderer sprite;
     
     void Update()
     {
-        if (Input.GetButton(ActivateGateA))
+        var openGateA = flipControls ? Input.GetButton(ActivateGateB) : Input.GetButton(ActivateGateA);
+        var openGateB = flipControls ? Input.GetButton(ActivateGateA) : Input.GetButton(ActivateGateB);
+
+        if (openGateA)
         {
             sprite.color = ColorA;
-            gate.gameObject.layer = 8;
+            gate.gameObject.layer = passLayerA;
+            blocker.gameObject.layer = blockLayerA;
         }
-        else if (Input.GetButton(ActivateGateB))
+        else if (openGateB)
         {
             sprite.color = ColorB;
-            gate.gameObject.layer = 9;
+            gate.gameObject.layer = passLayerB;
+            blocker.gameObject.layer = blockLayerB;
         }
         else
         {
             sprite.color = ColorDefault;
-            gate.gameObject.layer = 0;
+            gate.gameObject.layer = passLayerDefault;
+            blocker.gameObject.layer = blockLayerDefault;
         }
 
     }
